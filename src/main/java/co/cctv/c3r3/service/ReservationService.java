d8d53688017316d1778a6cbdbf2400a8c6e5e9bb
camilo <camilo@camilo-HP-Laptop-15-dw1xxx>
2021-11-05 15:24:49 -0500

    Reportes de Reto 5 Probados: Comentarios en reservacion service buenas practicas

--- src/main/java/co/cctv/c3r3/service/ReservationService.java
+++ src/main/java/co/cctv/c3r3/service/ReservationService.java
@@ -52,31 +52,31 @@ public class ReservationService {
     }
     
     public Reservation update(Reservation reservation){ //Metodo para actualizar una reservaciṕn.
-        if(reservation.getIdReservation()!=null){ //Valido si existe.
-            return reservationRepository.save(reservation);
+        if(reservation.getIdReservation()!=null){ //Valido si el Id es diferente de nulo y lo guardo como registro
+            return reservationRepository.save(reservation); //Guardo el registro. 
         }else{
             Optional<Reservation> e=reservationRepository.getReservation(reservation.getIdReservation());
             if(!e.isEmpty()){
-                if(reservation.getStartDate()!=null){
+                if(reservation.getStartDate()!=null){//Obtengo la fecha inicial
                     e.get().setStartDate(reservation.getStartDate());
                 }
-                if(reservation.getDevolutionDate()!=null){
+                if(reservation.getDevolutionDate()!=null){//Obtengo la fecha final
                     e.get().setDevolutionDate(reservation.getDevolutionDate());
                 }
-                if(reservation.getClient()!=null){
+                if(reservation.getClient()!=null){//Obtengo el objeto cliente
                     e.get().setClient(reservation.getClient());
                 }
-                reservationRepository.save(e.get());
+                reservationRepository.save(e.get()); //Guardo la edición
                 return e.get();
             }else{
-                return reservation;
+                return reservation;//Devuelvo reservación
             }
         }
     }
     
     public boolean deleteReservation(int id){ //Método para borrar una reservación. 
         
-        Boolean aBoolean=getReservation(id).map(reservation -> {
+        Boolean aBoolean=getReservation(id).map(reservation -> {//Busco la reservación por ID
             reservationRepository.delete(reservation);
             return true;
         }).orElse(aBoolean=false);
@@ -84,43 +84,45 @@ public class ReservationService {
         return aBoolean;
     }
     
-    public List<Reservation> getReservationsPeriod(String dateA, String dateB){
+    public List<Reservation> getReservationsPeriod(String dateA, String dateB){//ENlisto todas las reservaciones en un período de tiempo
         SimpleDateFormat parser=new SimpleDateFormat ("yyyy-MM-dd");
         Date a=new Date();
         Date b=new Date();
         try{
-            a=parser.parse(dateA);
+            a=parser.parse(dateA);//parseo las fechas
             b=parser.parse(dateB);
         }catch (ParseException e){
             e.printStackTrace();
         }
         
         if(a.before(b)){
-            return reservationRepository.getReservationPeriod(a,b);
+            return reservationRepository.getReservationPeriod(a,b); //Ejecuto la clase repositorio trayendo todas las reservaciones en el intervalo de fechas
         }else{
-            return new ArrayList<>();
+            return new ArrayList<>(); 
         }
     }
     
     public StatusCount getStatusReport(){
+        //Obtengo el estado de las reservaciones usando psql en la clase respositorio usando un string
         List<Reservation> completed=reservationRepository.getReservationByStatus("completed");
         List<Reservation> cancelled=reservationRepository.getReservationByStatus("cancelled");
 
+        //Cuento cuantas reservaciones están en estado String Completado y Cancelado
         StatusCount descAmt=new StatusCount(completed.size(),cancelled.size());
         return descAmt;
     }
     
-    public List<CountClient> getReservationByClient(){
-        List<CountClient> resultado=new ArrayList<>(); //Lista 
+    public List<CountClient> getReservationByClient(){//Cuento el número de reservaciones por cliente
+        List<CountClient> resultado=new ArrayList<>(); //Lista de cliente
         List<Object[]> report=reservationRepository.getReservationByClient(); //Objeto a recorrer
         for (int i = 0; i < report.size(); i++) {
             long val1=(long) report.get(i)[1];
             int value1=(int) val1;
             Client value2=(Client) report.get(i)[0];
-            resultado.add(new CountClient(value1,value2));
+            resultado.add(new CountClient(value1,value2));//agrego la cuenta al resultado
             
         }
-        return resultado;
+        return resultado;//devuelvo el resultado. Objeto con la cuenta de reservaciones y las resaervaciones todo como un objeto
     }
     
 }
